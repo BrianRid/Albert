161bb2ca86fb09ac4b66a236099ad1ff24a9f2ec
BrianRid <brianridolce@gmail.com>
2023-10-22 20:03:20 +0200

    fix: typescript error

--- src/context/AppContext.ts
+++ src/context/AppContext.ts
@@ -2,7 +2,7 @@ import { User } from "@prisma/client";
 import { SetStateAction, createContext } from "react";
 
 interface AppContextInterface {
-  user: User | undefined;
+  user: User | undefined | null;
 }
 
 const AppContext = createContext<AppContextInterface>({
