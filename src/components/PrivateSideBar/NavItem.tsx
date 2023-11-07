75d9dc9b7e79331212b1b39b417c3e4eeafe89a6
BrianRid <brianridolce@gmail.com>
2023-11-07 19:10:12 +0100

    feat: styling navitem

--- src/components/PrivateSideBar/NavItem.tsx
+++ src/components/PrivateSideBar/NavItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Box, Flex, Icon, type FlexProps } from "@chakra-ui/react";
 import { type IconType } from "react-icons";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface NavItemProps extends FlexProps {
   icon: IconType;
@@ -9,23 +11,22 @@ interface NavItemProps extends FlexProps {
 }
 
 const NavItem = ({ href, icon, children, ...rest }: NavItemProps) => {
+  const router = useRouter();
+  const isActive = href === router.pathname;
+
   return (
-    <Box
-      as="a"
-      href={href}
-      style={{ textDecoration: "none" }}
-      _focus={{ boxShadow: "none" }}
-    >
+    <Link href={href} style={{ textDecoration: "none" }}>
       <Flex
         align="center"
         p="4"
         mx="4"
         borderRadius="lg"
         role="group"
+        bg={isActive ? "blue.50" : "transparent"}
         cursor="pointer"
         _hover={{
-          bg: "gray.400",
-          color: "white",
+          bg: "blue.50",
+          color: "blue.900",
         }}
         {...rest}
       >
@@ -33,15 +34,16 @@ const NavItem = ({ href, icon, children, ...rest }: NavItemProps) => {
           <Icon
             mr="4"
             fontSize="16"
+            color={isActive ? "blue.900" : "blue.900"}
             _groupHover={{
-              color: "white",
+              color: "blue.900",
             }}
             as={icon}
           />
         )}
         {children}
       </Flex>
-    </Box>
+    </Link>
   );
 };
 
